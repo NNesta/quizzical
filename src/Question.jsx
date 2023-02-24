f05faa26db690452898e5fb99218801eb5e2a196
Ngabonziza Nestor <ngabonest@gmail.com>
2023-02-24 17:00:41 +0200

    feat:final desktop app

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,26 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import QuestionCard from './components/QuestionCard';
-import data from './data';
+const _ = require('lodash');
+
 
 const Question = () => {
   const [checked, setChecked] = useState(false);
-  const [passed, setPassed] = useState(0);
+  const [data, setData] = useState([])
+  const [newData, setNewData] = useState([])
+  useEffect(()=>{
+    fetch('https://opentdb.com/api.php?amount=5&category=23&type=multiple').then(re=>re.json()).then(result=>setNewData(result.results.map((item,index)=>({question:item.question, choices:_.shuffle([...item.incorrect_answers,item.correct_answer]), correct_answer:item.correct_answer, id:index})))
+    )
+  },[])
+
+  const [success, setSuccess] = useState(Array.from({length:5}));
+  console.log(success)
+  // console.log(data.results&&data.results[0])
   return (
-    <main className="bg-secondary-100 flex flex-col gap-2 px-32 justify-center text-primary-100 max-w-[750px] mx-auto my-20 aspect-square relative">
+    <main className="bg-secondary-100 flex flex-col gap-2 px-32 justify-center text-primary-100 w-full h-fit mx-auto  relative">
       <img className="absolute top-0 right-0" src="/blobs.svg"/>
       <img className="absolute bottom-0 left-0" src="/blobs_2.svg"/>
      <div className="flex flex-col my-4  gap-4 ">
     {
-      data.map((item, index) =>  <QuestionCard key={index} question={item.question} choices={item.choices} correct={item.correct} checked={checked} setPassed={setPassed}/>)
+      newData.map((item, index) =>  <QuestionCard key={index} question={item.question} choices={item.choices} correct_answer={item.correct_answer} id={item.id}   checked={checked} setSuccess={setSuccess} />)
         
       
     }
      </div>
    {  checked?<div className='flex items-center gap-16 w-[354px] mx-auto'>
-      <h1 className='text-primary-100 font-[700] text-[12.8px] leading-[15.49px]'><span>You scored</span> <span>{passed}/{data.length}</span> <span>correct answers</span> </h1>
+      <h1 className='text-primary-100 font-[700] text-[12.8px] leading-[15.49px]'><span>You scored</span> <span>{success.filter(x=>x).length}/{newData.length}</span> <span>correct answers</span> </h1>
       <button onClick={()=>window.location.reload(false)} className='text-white bg-primary-200 rounded-[10px] px-2 py-1'>Play again</button>
      </div>:<div className='flex items-center gap-16 w-[354px] mx-auto'>
-     <button onClick={()=>setChecked(true)} className='text-white bg-primary-200 rounded-[10px] px-2 py-1'>Check answers</button>
+     <button onClick={()=>{setChecked(true);}} className='text-white bg-primary-200 rounded-[10px] px-2 py-1'>Check answers</button>
 
       </div>}
     </main>
--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,17 +1,15 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 
-const QuestionCard = ({question, choices, correct, checked,setPassed}) => {
+const QuestionCard = ({question,choices, correct_answer, id, checked,setSuccess}) => {
   const [submitted, setSubmitted] = useState(null);
-  // if(checked&&correct==choices[submitted]){
-  //   console.log(submitted)
-  //  setPassed(prev=>prev+1) 
-  // }
-  console.log(checked)
+ 
   return (
-    <div className='w-[399px] flex flex-col gap-2 border-b-[1px] py-4 px-2'>
+    <div className='w-[80%] flex flex-col gap-2 border-b-[1px] py-4 px-2'>
     <h1>{question}</h1>
     <ul className='flex items-center gap-8'>
-      {choices.map((choice, index) => <li key={index} className={`border whitespace-nowrap border-primary-100 rounded-[7.94px] px-2 py-1 ${submitted==index&&!checked&&"bg-secondary-200"} ${correct===choice&&checked&&"bg-success-100"} ${submitted==index&&correct!==choice&&checked&&"bg-danger-100"} `} disabled={checked}><button onClick={()=>setSubmitted(index)} className="text-[16px] text-primary-100">{choice}</button></li>)}
+      {choices.map((choice, index) => <li key={index} className={`border whitespace-nowrap border-primary-100  rounded-[7.94px] px-2 py-1 ${submitted==index&&!checked&&"bg-secondary-200"} ${choice===correct_answer&&checked&&"bg-success-100"} ${submitted==index&&correct_answer!==choice&&checked&&"bg-danger-100"} `} >
+        <button disabled={checked} onClick={()=>{setSubmitted(index);
+         setSuccess(prev=>{prev[id] = choice===correct_answer; return prev })}} className="text-[16px] text-primary-100">{choice}</button></li>)}
     </ul>
     </div>
   )
